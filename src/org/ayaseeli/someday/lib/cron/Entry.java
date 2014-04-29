053655378fb9883f0461b013690e13a917274087
Misumi Rize <r@ayase-e.li>
2014-04-29 15:44:30 +0900

    entry parsing

--- src/org/ayaseeli/someday/lib/cron/Entry.java
+++ src/org/ayaseeli/someday/lib/cron/Entry.java
@@ -4,48 +4,74 @@ import java.util.Calendar;
 
 class Entry {
 
-    private Calendar now;
     private MinuteField minuteField;
     private HourField hourField;
     private DayField dayField;
     private MonthField monthField;
     private WeekField weekField;
+    private Calendar now;
 
-    Entry(Calendar now, Operator minute, Operator hour, Operator day, Operator month, Operator week) {
-        this.now = now;
+    private Entry(Operator minute, Operator hour, Operator day, Operator month, Operator week, Calendar now) {
         minuteField = new MinuteField(minute);
         hourField = new HourField(hour);
         dayField = new DayField(day);
         monthField = new MonthField(month);
         weekField = new WeekField(week);
+        this.now = now;
+    }
+
+    static Entry parse(String entry) {
+        return parse(entry, Calendar.getInstance());
+    }
+
+    static Entry parse(String entry, Calendar now) {
+        String[] operators = entry.split(" ");
+        if (operators.length != 5) {
+            throw new IllegalEntryException();
+        }
+        Calendar startAt = (Calendar) now.clone();
+        startAt.add(Calendar.MINUTE, 1);
+        startAt.set(Calendar.SECOND, 0);
+        startAt.set(Calendar.MILLISECOND, 0);
+        return new Entry(Operator.parse(operators[0], startAt.get(Calendar.MINUTE)),
+                Operator.parse(operators[1], startAt.get(Calendar.HOUR_OF_DAY)),
+                Operator.parse(operators[2], startAt.get(Calendar.DATE)),
+                Operator.parse(operators[3], startAt.get(Calendar.MONTH) + 1),
+                Operator.parse(operators[4], startAt.get(Calendar.DAY_OF_WEEK)), now);
     }
 
     Calendar next() {
-        now.add(Calendar.MINUTE, 1);
-        now.set(Calendar.SECOND, 0);
-        now.set(Calendar.MILLISECOND, 0);
+        now = next(now);
+        return (Calendar) now.clone();
+    }
+
+    private Calendar next(Calendar now) {
+        Calendar next = (Calendar) now.clone();
+        next.add(Calendar.MINUTE, 1);
+        next.set(Calendar.SECOND, 0);
+        next.set(Calendar.MILLISECOND, 0);
         while (true) {
-            if (!monthField.isSatisfiedWith(now)) {
-                now = monthField.postpone(now);
+            if (!monthField.isSatisfiedWith(next)) {
+                next = monthField.postpone(next);
                 continue;
             }
-            if (!weekField.isSatisfiedWith(now)) {
-                now = weekField.postpone(now);
+            if (!weekField.isSatisfiedWith(next)) {
+                next = weekField.postpone(next);
                 continue;
             }
-            if (!dayField.isSatisfiedWith(now)) {
-                now = dayField.postpone(now);
+            if (!dayField.isSatisfiedWith(next)) {
+                next = dayField.postpone(next);
                 continue;
             }
-            if (!hourField.isSatisfiedWith(now)) {
-                now = hourField.postpone(now);
+            if (!hourField.isSatisfiedWith(next)) {
+                next = hourField.postpone(next);
                 continue;
             }
-            if (!minuteField.isSatisfiedWith(now)) {
-                now = minuteField.postpone(now);
+            if (!minuteField.isSatisfiedWith(next)) {
+                next = minuteField.postpone(next);
                 continue;
             }
-            return now;
+            return next;
         }
     }
 
--- test/org/ayaseeli/someday/lib/cron/EntryTest.java
+++ test/org/ayaseeli/someday/lib/cron/EntryTest.java
@@ -10,29 +10,33 @@ import org.junit.Test;
 public class EntryTest {
 
     private Calendar now;
-    private Operator every;
 
     @Before
     public void setUp() throws Exception {
         now = Calendar.getInstance();
         now.set(2014, 4, 26, 10, 24, 15);
-        every = IntervalOperator.parse("*", 1);
     }
 
     @Test
     public void testNext() {
-        Entry entry = new Entry(now, every, every, every, every, every);
+        Entry entry = Entry.parse("* * * * *", now);
         Calendar next = entry.next();
         assertEquals(next.get(Calendar.MINUTE), 25);
     }
 
     @Test
     public void testNext_willCarryHour() {
-        Operator operator = new Operator("15");
-        Entry entry = new Entry(now, every, operator, every, every, every);
+        Entry entry = Entry.parse("4,10 15 * * *", now);
         Calendar next = entry.next();
         assertEquals(next.get(Calendar.HOUR_OF_DAY), 15);
-        assertEquals(next.get(Calendar.MINUTE), 0);
+        assertEquals(next.get(Calendar.MINUTE), 4);
+        Calendar next2 = entry.next();
+        assertEquals(next2.get(Calendar.HOUR_OF_DAY), 15);
+        assertEquals(next2.get(Calendar.MINUTE), 10);
+        Calendar next3 = entry.next();
+        assertEquals(next3.get(Calendar.DATE), 27);
+        assertEquals(next3.get(Calendar.HOUR_OF_DAY), 15);
+        assertEquals(next3.get(Calendar.MINUTE), 4);
     }
-
+    
 }
